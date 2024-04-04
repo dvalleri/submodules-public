bf484c4f52d142d5d6c9d10f39fdc0cc1a68bd8c
Davide Valleri <davide.valleri@radicalbit.io>
2024-04-04 15:40:17 +0200

    add header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
@@ -17,7 +18,10 @@ function App() {
         >
           Learn React
         </a>
+        <Header></Header>
       </header>
+
+
     </div>
   );
 }
---
+++ src/components/header/index.jsx
@@ -0,0 +1,13 @@
+ import "./style.css"
+ 
+ function Header(){
+    return (
+        <div className="flex-row align-self justify-between w-100 m-10">
+            <h3>Bacon ipsum dolor</h3>
+            <h4>amet flank shankle</h4>
+            <h3>pork loin pig</h3>
+        </div>
+    )
+ }
+
+ export default Header
